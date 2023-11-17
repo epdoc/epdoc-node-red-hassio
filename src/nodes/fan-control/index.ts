12b3ee18756af6a5487d8c1524f09cfaaca30601
Jim Pravetz <jpravetz@gmail.com>
2023-11-17 17:46:34 -0600

    Opting to not use gulp to build html files, so adding more code back into html.

--- src/nodes/fan-control/index.ts
+++ src/nodes/fan-control/index.ts
@@ -1,9 +1,7 @@
-import { NodeAPI, NodeMessage } from 'node-red';
+import { Node, NodeAPI, NodeMessage } from 'node-red';
 
 import { NodeRedDoneFunction, NodeRedSendFunction } from 'epdoc-node-red-hautil';
-import { RED, setRED } from '../../globals';
-import Status from '../common/Status';
-import NodeRedContextService from '../common/context-service';
+import { setRED } from '../../globals';
 import { BaseNode } from '../common/types';
 import { FanControl } from './fan-control';
 import { FanControlNodeConfig, isFanControlNodeConfig } from './types';
@@ -13,57 +11,44 @@ export interface FanControlNode extends BaseNode {
   controller: FanControl;
 }
 
-export default function fanControlNode(this: FanControlNode, config: FanControlNodeConfig): void {
-  RED.nodes.createNode(this, config);
-  // this.config = migrate(config);
-
-  // const serverConfigNode = getServerConfigNode(this.config.server);
-  // const homeAssistant = getHomeAssistant(serverConfigNode);
-  const exposeAsConfigNode = false; // getExposeAsConfigNode(this.config.exposeAsEntityConfig);
-  // const clientEvents = new ClientEvents({
-  //   node: this,
-  //   emitter: homeAssistant.eventBus
-  // });
-  const status = new Status({ node: this });
-
-  const nodeRedContextService = new NodeRedContextService(this);
-
-  try {
-    console.log('FanControlNode');
-    RED.nodes.createNode(this, config);
-    let node: FanControlNode = this as FanControlNode;
-
-    if (!isFanControlNodeConfig(config)) {
-      return;
-    }
-    // this.server = RED.nodes.getNode(config.server);
-    // if (this.server) {
-    // }
-
-    // const processMsg = async (msg: NodeMessage, send: NodeRedSendFunction, done: NodeRedDoneFunction) => {
-    //   console.log('process message');
-    //};
-    const processMsg = async (msg: NodeMessage, send: NodeRedSendFunction, done: NodeRedDoneFunction) => {
-      const fanCtrl = new FanControl(node, msg, send, done);
-      fanCtrl.setUiConfig(config);
-      fanCtrl.setPayloadConfig(msg.payload);
-      fanCtrl.run().then((resp) => {
-        fanCtrl.done();
-      });
-    };
-
-    const done = () => {
-      console.log('done');
-    };
-
-    this.on('input', processMsg);
-    this.on('close', done);
-  } catch (err) {
-    console.log(err);
-  }
-}
-
 module.exports = function (RED: NodeAPI) {
   setRED(RED);
+  function fanControlNode(config: FanControlNodeConfig): void {
+    console.log('FanControlNode');
+    // @ts-ignore
+    RED.nodes.createNode(this as Node, config);
+    // @ts-ignore
+    let node: FanControlNode = this as FanControlNode;
+
+    // const exposeAsConfigNode = false; // getExposeAsConfigNode(this.config.exposeAsEntityConfig);
+
+    // const status = new Status({ node: node });
+
+    // const nodeRedContextService = new NodeRedContextService(node);
+
+    try {
+      if (!isFanControlNodeConfig(config)) {
+        return;
+      }
+      const processMsg = async (msg: NodeMessage, send: NodeRedSendFunction, done: NodeRedDoneFunction) => {
+        const fanCtrl = new FanControl(node, msg, send, done);
+        fanCtrl.setUiConfig(config);
+        fanCtrl.setPayloadConfig(msg.payload);
+        fanCtrl.run().then((resp) => {
+          fanCtrl.done();
+        });
+      };
+
+      const done = () => {
+        console.log('done');
+      };
+
+      node.on('input', processMsg);
+      node.on('close', done);
+    } catch (err) {
+      console.log(err);
+    }
+  }
+
   RED.nodes.registerType('fan-control', fanControlNode);
 };
