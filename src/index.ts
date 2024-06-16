4f9587fbe35b697746857648c3da7223ec266282
Jim Pravetz <jpravetz@gmail.com>
2024-06-15 18:01:45 -0600

    Removed bug loading package.json

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,5 @@
-import pkg from '../package.json';
-
 export * from './factory';
 export * from './index';
 export * from './nodes/fan-control/fan-control';
 export * from './nodes/location-history';
 export * from './nodes/ping-test/ping-context';
-
-export function version(): string {
-  return pkg.version;
-}
